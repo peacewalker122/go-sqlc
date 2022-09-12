059d8b16c3ca46ef869cbcade01d32d66df585c1
peacewalker122 <danielangrh@gmail.com>
2022-09-12 21:19:13 +0800

    dummy/update

--- dummy/dummy.go
+++ dummy/dummy.go
@@ -5,7 +5,9 @@ import (
 	"strings"
 	"time"
 )
+
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
@@ -13,30 +15,31 @@ func init() {
 func randomint(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
 }
-func randomstring(n int) string{
+func randomstring(n int) string {
 	var sb strings.Builder
 	l := len(alphabet)
-		for i:=0;i<l;i++{
-			c := alphabet[rand.Intn(l)]
-			sb.WriteByte(c)
-		}
+	for i := 0; i < l; i++ {
+		c := alphabet[rand.Intn(l)]
+		sb.WriteByte(c)
+	}
 	return sb.String()
 }
 
-func Randomowner() string{
+func Randomowner() string {
 	return randomstring(6)
 }
 
 func Randommoney() int64 {
-	return randomint(0,1000)
+	return randomint(0, 1000)
 }
 
-func Randomcurrency() string{
+func Randomcurrency() string {
 	c := []string{"IDR", "USD", "GBP", "EUR"}
 	l := len(c)
 	return c[rand.Intn(l)]
 }
 
+// it's nothing just a dummy to test the git
 func Nothing() {
 	println("nothing")
 }
